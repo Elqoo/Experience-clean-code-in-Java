27bfe847d055c87cf6972386ecd6ae189e234ae2
Stijn De Mulder <stijn.demulder@elqoo.com>
2017-12-15 10:35:17 +0100

    Add identation rule

--- src/test/java/academy/elqoo/cleancode/methods/MethodsTest.java
+++ src/test/java/academy/elqoo/cleancode/methods/MethodsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class MethodsTest {
 
@@ -12,6 +13,12 @@ public class MethodsTest {
         // optimize the prime class so it follows the abstraction level rule
         int[] primes = {2, 3, 5, 7, 11};
         assertThat(primes, equalTo(Prime.generatePrimeNumbersUntil(12)));
+    }
+
+    @Test
+    public void shouldUseMaxLevelThreeIdentation(){
+        // the Identation class should be refactored to limit the number of identations
+        assertTrue(Identation.isABigYellowBanana("banana",true,"yellow"));
     }
 
 }
